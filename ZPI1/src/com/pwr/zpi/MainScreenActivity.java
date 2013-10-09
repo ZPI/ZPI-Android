59b5333c192dc168de42471e392552c4f9049e3b
Kuba <Kuba@Kuba-Komputer>
2013-10-09 08:46:18 +0200

    GPS Text icon GPS settings are now enabled on GPSStatusTextClick

--- ZPI1/src/com/pwr/zpi/MainScreenActivity.java
+++ ZPI1/src/com/pwr/zpi/MainScreenActivity.java
@@ -9,20 +9,26 @@ import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainScreenActivity extends Activity {
+public class MainScreenActivity extends Activity implements OnClickListener{
 
 	TextView GPSStatusTextView;
-
+	private static final short GPS_NOT_ENABLED = 0;
+	private static final short NO_GPS_SIGNAL = 1;
+	private static final short GPS_WORKING = 2;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_screen_activity);
 
 		GPSStatusTextView = (TextView) findViewById(R.id.textViewGPSIndicator);
-
+		GPSStatusTextView.setOnClickListener(this);
+			
+		
 		prepareGestureListener();
 		addListeners();
 	}
@@ -45,6 +51,7 @@ public class MainScreenActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		checkGPS();
+		Toast.makeText(this, "onResume", Toast.LENGTH_LONG);
 	}
 
 	@Override
@@ -58,7 +65,10 @@ public class MainScreenActivity extends Activity {
 		return gestureListener.onTouch(null, event);
 	}
 
-	private void checkGPS() {
+	private short checkGPS() {
+		
+		short gpsStatus = 0;
+		
 		LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
 		boolean enabled = service
 				.isProviderEnabled(LocationManager.GPS_PROVIDER);
@@ -66,13 +76,15 @@ public class MainScreenActivity extends Activity {
 		if (!enabled) {
 			GPSStatusTextView.setText(getResources().getString(
 					R.string.gps_disabled));
-			// Intent intent = new
-			// Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-			// startActivity(intent);
+			gpsStatus = GPS_NOT_ENABLED;
+			
 		} else {
 			GPSStatusTextView.setText(getResources().getString(
 					R.string.gps_enabled));
+			gpsStatus = NO_GPS_SIGNAL;
+			
 		}
+		return gpsStatus;
 	}
 
 	private static final int SWIPE_MIN_DISTANCE = 120;
@@ -111,6 +123,22 @@ public class MainScreenActivity extends Activity {
 			return false;
 		}
 
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		if (v == GPSStatusTextView)
+		{
+			//if gps is not running
+			if (checkGPS() == GPS_NOT_ENABLED)
+			{
+				Intent intent = new
+				Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				startActivity(intent);
+			}
+		}
+		
 	}
 
 }
