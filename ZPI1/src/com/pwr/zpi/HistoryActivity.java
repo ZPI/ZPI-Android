7d59552d3210dece4562399912d6926ba690dcf1
Kuba <Kuba@Kuba-Komputer>
2013-10-09 19:25:18 +0200

    back swipes

--- ZPI1/src/com/pwr/zpi/HistoryActivity.java
+++ ZPI1/src/com/pwr/zpi/HistoryActivity.java
@@ -1,21 +1,72 @@
 package com.pwr.zpi;
 
+import com.pwr.zpi.listeners.GestureListener;
+import com.pwr.zpi.listeners.MyGestureDetector;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class HistoryActivity extends Activity {
+public class HistoryActivity extends Activity implements GestureListener{
 
+	GestureDetector gestureDetector;
+	private View.OnTouchListener gestureListener;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.history_activity);
+		prepareGestureListener();
+		
 	}
 
+	private void prepareGestureListener() {
+		// Gesture detection
+		gestureDetector = new GestureDetector(this, new MyGestureDetector(this, false, false, true, false));
+		gestureListener = new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				return gestureDetector.onTouchEvent(event);
+			}
+		};
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return gestureListener.onTouch(null, event);
+	}
+	
 	@Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.in_right_anim, R.anim.out_right_anim);
 
     }
+
+	@Override
+	public void onLeftToRightSwipe() {
+		
+		finish();
+		overridePendingTransition(R.anim.in_right_anim, R.anim.out_right_anim);
+	}
+
+	@Override
+	public void onRightToLeftSwipe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpToDownSwipe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDownToUpSwipe() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
--- ZPI1/src/com/pwr/zpi/SettingsActivity.java
+++ ZPI1/src/com/pwr/zpi/SettingsActivity.java
@@ -1,20 +1,70 @@
 package com.pwr.zpi;
 
+import com.pwr.zpi.listeners.GestureListener;
+import com.pwr.zpi.listeners.MyGestureDetector;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity implements GestureListener {
 
+	GestureDetector gestureDetector;
+	private View.OnTouchListener gestureListener;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings_activity);
+		prepareGestureListener();
 	}
 
 	@Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.in_down_anim, R.anim.out_down_anim);
+	}
+	
+	private void prepareGestureListener() {
+		// Gesture detection
+		gestureDetector = new GestureDetector(this, new MyGestureDetector(this, false, true, false, false));
+		gestureListener = new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				return gestureDetector.onTouchEvent(event);
+			}
+		};
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return gestureListener.onTouch(null, event);
+	}
+	
+	@Override
+	public void onLeftToRightSwipe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRightToLeftSwipe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpToDownSwipe() {
+		
+		finish();
+		overridePendingTransition(R.anim.in_down_anim,R.anim.out_down_anim);
+	}
+
+	@Override
+	public void onDownToUpSwipe() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
