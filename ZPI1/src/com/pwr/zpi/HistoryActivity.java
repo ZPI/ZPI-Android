731033911eca3f084060969c1b645b2a688318f6
Marek Kreśnicki <marek.kresnicki@gmail.com>
2013-10-09 08:24:58 +0200

    Activities

---
+++ ZPI1/src/com/pwr/zpi/HistoryActivity.java
@@ -0,0 +1,14 @@
+package com.pwr.zpi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HistoryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.history_activity);
+	}
+
+}
