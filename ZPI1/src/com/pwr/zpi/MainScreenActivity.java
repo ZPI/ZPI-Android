95cd979fc9366c790fb515b248383d3cc45cbf0c
Marek Kreśnicki <marek.kresnicki@gmail.com>
2013-10-11 16:52:14 +0200

    all |

--- ZPI1/src/com/pwr/zpi/MainScreenActivity.java
+++ ZPI1/src/com/pwr/zpi/MainScreenActivity.java
@@ -8,7 +8,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
@@ -19,26 +21,27 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainScreenActivity extends Activity implements OnClickListener, GestureListener {
+public class MainScreenActivity extends Activity implements OnClickListener,
+		GestureListener {
 
 	private TextView GPSStatusTextView;
 	private Button settingsButton;
 	private VerticalTextView historyButton;
 	private VerticalTextView planningButton;
 	private Button startButton;
-	
+	private Button musicButton;
+
 	private GestureDetector gestureDetector;
 	private View.OnTouchListener gestureListener;
-	
+
 	private static final short GPS_NOT_ENABLED = 0;
 	private static final short NO_GPS_SIGNAL = 1;
 	private static final short GPS_WORKING = 2;
-	
+
 	private static final short LEFT = 0;
 	private static final short RIGHT = 1;
 	private static final short UP = 2;
 	private static final short DOWN = 3;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +53,8 @@ public class MainScreenActivity extends Activity implements OnClickListener, Ges
 		historyButton = (VerticalTextView) findViewById(R.id.buttonHistory);
 		planningButton = (VerticalTextView) findViewById(R.id.buttonPlans);
 		startButton = (Button) findViewById(R.id.buttonStart);
-		
+		musicButton = (Button) findViewById(R.id.buttonMusic);
+
 		prepareGestureListener();
 		addListeners();
 	}
@@ -61,8 +65,9 @@ public class MainScreenActivity extends Activity implements OnClickListener, Ges
 		historyButton.setOnClickListener(this);
 		startButton.setOnClickListener(this);
 		planningButton.setOnClickListener(this);
-		//TODO add listeners to buttons so that swipe will work
-		
+		musicButton.setOnClickListener(this);
+		// TODO add listeners to buttons so that swipe will work
+
 		GPSStatusTextView.setOnTouchListener(gestureListener);
 		settingsButton.setOnTouchListener(gestureListener);
 		historyButton.setOnTouchListener(gestureListener);
@@ -72,7 +77,8 @@ public class MainScreenActivity extends Activity implements OnClickListener, Ges
 
 	private void prepareGestureListener() {
 		// Gesture detection
-		gestureDetector = new GestureDetector(this, new MyGestureDetector(this, true, true, true, true));
+		gestureDetector = new GestureDetector(this, new MyGestureDetector(this,
+				true, true, true, true));
 		gestureListener = new View.OnTouchListener() {
 			public boolean onTouch(View v, MotionEvent event) {
 				return gestureDetector.onTouchEvent(event);
@@ -97,12 +103,11 @@ public class MainScreenActivity extends Activity implements OnClickListener, Ges
 		return gestureListener.onTouch(null, event);
 	}
 
-	
-	//TODO check if gps has signal
+	// TODO check if gps has signal
 	private short checkGPS() {
-		
+
 		short gpsStatus = 0;
-		
+
 		LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
 		boolean enabled = service
 				.isProviderEnabled(LocationManager.GPS_PROVIDER);
@@ -111,106 +116,104 @@ public class MainScreenActivity extends Activity implements OnClickListener, Ges
 			GPSStatusTextView.setText(getResources().getString(
 					R.string.gps_disabled));
 			gpsStatus = GPS_NOT_ENABLED;
-			
+
 		} else {
 			GPSStatusTextView.setText(getResources().getString(
 					R.string.gps_enabled));
 			gpsStatus = NO_GPS_SIGNAL;
-			
+
 		}
 		return gpsStatus;
 	}
 
-	private void startActivity(Class<? extends Activity> activity, final short swipeDirection) {
+	private void startActivity(Class<? extends Activity> activity,
+			final short swipeDirection) {
 		final Intent i = new Intent(MainScreenActivity.this, activity);
-		
-		
-		AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+
+		AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
 
 			@Override
 			protected void onPreExecute() {
 				super.onPreExecute();
-				Toast.makeText(MainScreenActivity.this, "loading", Toast.LENGTH_SHORT).show();
+				Toast.makeText(MainScreenActivity.this, "loading",
+						Toast.LENGTH_SHORT).show();
 			}
 
 			@Override
 			protected Void doInBackground(Void... params) {
 				startActivity(i);
-				if (swipeDirection == RIGHT)
-				{
-					overridePendingTransition(R.anim.in_right_anim,R.anim.out_right_anim);
-				}
-				else if (swipeDirection == LEFT)
-				{
-					overridePendingTransition(R.anim.in_left_anim,R.anim.out_left_anim);
-				}
-				else if (swipeDirection == DOWN)
-				{
-					overridePendingTransition(R.anim.in_down_anim,R.anim.out_down_anim);
-				}
-				else if (swipeDirection == UP)
-				{
-					overridePendingTransition(R.anim.in_up_anim,R.anim.out_up_anim);
+				if (swipeDirection == RIGHT) {
+					overridePendingTransition(R.anim.in_right_anim,
+							R.anim.out_right_anim);
+				} else if (swipeDirection == LEFT) {
+					overridePendingTransition(R.anim.in_left_anim,
+							R.anim.out_left_anim);
+				} else if (swipeDirection == DOWN) {
+					overridePendingTransition(R.anim.in_down_anim,
+							R.anim.out_down_anim);
+				} else if (swipeDirection == UP) {
+					overridePendingTransition(R.anim.in_up_anim,
+							R.anim.out_up_anim);
 				}
 				return null;
 			}
-			
+
 		}.execute();
 	}
 
 	@Override
 	public void onLeftToRightSwipe() {
-		startActivity(PlaningActivity.class,RIGHT);
+		startActivity(PlaningActivity.class, RIGHT);
 
 	}
 
 	@Override
 	public void onRightToLeftSwipe() {
-		startActivity(HistoryActivity.class,LEFT);
+		startActivity(HistoryActivity.class, LEFT);
 
 	}
 
 	@Override
 	public void onUpToDownSwipe() {
-		startActivity(ActivityActivity.class,DOWN);
+		startActivity(ActivityActivity.class, DOWN);
 
 	}
 
 	@Override
 	public void onDownToUpSwipe() {
-		startActivity(SettingsActivity.class,UP);
+		startActivity(SettingsActivity.class, UP);
 
 	}
+
 	@Override
 	public void onClick(View v) {
-		
-		if (v == GPSStatusTextView)
-		{
-			//if gps is not running
-			if (checkGPS() == GPS_NOT_ENABLED)
-			{
-				Intent intent = new	Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+
+		if (v == GPSStatusTextView) {
+			// if gps is not running
+			if (checkGPS() == GPS_NOT_ENABLED) {
+				Intent intent = new Intent(
+						Settings.ACTION_LOCATION_SOURCE_SETTINGS);
 				startActivity(intent);
 			}
-		}
-		else if (v == startButton)
-		{
-			startActivity(ActivityActivity.class,DOWN);
+		} else if (v == startButton) {
+			startActivity(ActivityActivity.class, DOWN);
 
-		}
-		else if (v == historyButton)
-		{
-			startActivity(HistoryActivity.class,LEFT);
+		} else if (v == historyButton) {
+			startActivity(HistoryActivity.class, LEFT);
 
+		} else if (v == settingsButton) {
+			startActivity(SettingsActivity.class, UP);
+
+		} else if (v == planningButton) {
+			startActivity(PlaningActivity.class, RIGHT);
+		} else if (v == musicButton) {
+			startSystemMusicPlayer();
 		}
-		else if (v == settingsButton)
-		{
-			startActivity(SettingsActivity.class,UP);
-			
-		}
-		else if (v == planningButton)
-		{
-			startActivity(PlaningActivity.class,RIGHT);
-		}
+	}
+
+	private void startSystemMusicPlayer() {
+		Intent i;
+		i = new Intent(MediaStore.INTENT_ACTION_MUSIC_PLAYER);
+		startActivity(i);
 	}
 }
