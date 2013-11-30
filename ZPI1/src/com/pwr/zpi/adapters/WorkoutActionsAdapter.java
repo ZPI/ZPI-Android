b0272a3e62bdd1f7e92afead8aaeed994b1d15a4
Kuba <Kuba@Kuba-Komputer>
2013-11-30 10:18:58 +0100

    action list working with setTag, I will try to do the same in drawer now

--- ZPI1/src/com/pwr/zpi/adapters/WorkoutActionsAdapter.java
+++ ZPI1/src/com/pwr/zpi/adapters/WorkoutActionsAdapter.java
@@ -28,6 +28,7 @@ public class WorkoutActionsAdapter extends ArrayAdapter<WorkoutAction> {
 	
 	public WorkoutActionsAdapter(Context context, int layoutResourceSimpleID, int layoutResourceAdvancedID,
 		List<WorkoutAction> data) {
+		
 		super(context, layoutResourceSimpleID, data);
 		this.layoutResourceSimpleID = layoutResourceSimpleID;
 		this.layoutResourceAdvancedID = layoutResourceAdvancedID;
@@ -35,79 +36,96 @@ public class WorkoutActionsAdapter extends ArrayAdapter<WorkoutAction> {
 	}
 	
 	@Override
+	public int getItemViewType(int position) {
+		WorkoutAction action = getItem(position);
+		if (action.isAdvanced()) return WorkoutAction.ACTION_ADVANCED;
+		else return WorkoutAction.ACTION_SIMPLE;
+		
+	}
+	
+	@Override
+	public int getViewTypeCount() {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+	
+	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View row = convertView;
 		WorkoutActionHolder holder = null;
 		WorkoutAction action = getItem(position);
-		// (row == null)
-		//	{
-		holder = new WorkoutActionHolder();
-		
-		LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-		if (action.isSimple()) {
-			row = inflater.inflate(layoutResourceSimpleID, parent, false);
-			holder.speedString = (TextView) row.findViewById(R.id.dataTextViewActionSpeedString);
-			holder.timeOrDistance = (TextView) row.findViewById(R.id.dataTextViewActionTimeOrDistance);
-			
-		}
-		else {
-			row = inflater.inflate(layoutResourceAdvancedID, parent, false);
-			holder.pace = (TextView) row.findViewById(R.id.dataTextViewActionPace);
-			holder.distance = (TextView) row.findViewById(R.id.dataTextViewActionDistance);
-			holder.time = (TextView) row.findViewById(R.id.dataTextViewActionTime);
-		}
-		
-		holder.image = (ImageView) row.findViewById(R.id.imageViewWorkoutAction);
-		
-		row.setTag(holder);
-		
-		//	}
-		//	else
-		//	{
-		//		holder = (WorkoutActionHolder) row.getTag();
-		//	}
-		
-		if (action.isAdvanced())
+		int type = getItemViewType(position);
+		if (row == null)
 		{
-			//TODO set image here
-			//holder.image.setImageResource(/*advanced image id*/);
-			WorkoutActionAdvanced advancedAction = (WorkoutActionAdvanced) action;
-			holder.time.setText(TimeFormatter.formatTimeHHMMSS(advancedAction.getTime()));
-			holder.pace.setText(TimeFormatter.formatTimeMMSSorHHMMSS(advancedAction.getPace()));
-			holder.distance.setText(String.format("%.3f", advancedAction.getDistance() / 1000));
+			holder = new WorkoutActionHolder();
 			
-		}
-		else if (action.isSimple())
-		{
-			//TODO set image here
-			//holder.image.setImageResource(/*advanced image id*/);
-			WorkoutActionSimple simpleAction = (WorkoutActionSimple) action;
-			int speedString = simpleAction.getSpeedType();
-			switch (speedString)
-			{
-				case WorkoutAction.ACTION_SIMPLE_SPEED_SLOW:
-					holder.speedString.setText(context.getResources().getString(R.string.slow));
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			
+			switch (type) {
+				case WorkoutAction.ACTION_SIMPLE:
+					row = inflater.inflate(layoutResourceSimpleID, parent, false);
+					holder.speedString = (TextView) row.findViewById(R.id.dataTextViewActionSpeedString);
+					holder.timeOrDistance = (TextView) row.findViewById(R.id.dataTextViewActionTimeOrDistance);
+					
 					break;
-				case WorkoutAction.ACTION_SIMPLE_SPEED_STEADY:
-					holder.speedString.setText(context.getResources().getString(R.string.steady));
-					break;
-				case WorkoutAction.ACTION_SIMPLE_SPEED_FAST:
-					holder.speedString.setText(context.getResources().getString(R.string.fast));
-					break;
-			}
-			int valueType = simpleAction.getValueType();
-			switch (valueType)
-			{
-				case WorkoutAction.ACTION_SIMPLE_VALUE_TYPE_DISTANCE:
-					holder.timeOrDistance.setText(String.format("%.3f%s", simpleAction.getValue() / 1000, context
-						.getResources().getString(R.string.km)));
-					break;
-				case WorkoutAction.ACTION_SIMPLE_VALUE_TYPE_TIME:
-					holder.timeOrDistance
-						.setText(TimeFormatter.formatTimeHHMMSS((long) (simpleAction.getValue())));
+				
+				case WorkoutAction.ACTION_ADVANCED:
+					row = inflater.inflate(layoutResourceAdvancedID, parent, false);
+					holder.pace = (TextView) row.findViewById(R.id.dataTextViewActionPace);
+					holder.distance = (TextView) row.findViewById(R.id.dataTextViewActionDistance);
+					holder.time = (TextView) row.findViewById(R.id.dataTextViewActionTime);
 					break;
 			}
 			
+			holder.image = (ImageView) row.findViewById(R.id.imageViewWorkoutAction);
+			
+			row.setTag(holder);
+			
+		}
+		else
+		{
+			holder = (WorkoutActionHolder) row.getTag();
+		}
+		switch (type) {
+			case WorkoutAction.ACTION_SIMPLE:
+				//TODO set image here
+				//holder.image.setImageResource(/*advanced image id*/);
+				WorkoutActionSimple simpleAction = (WorkoutActionSimple) action;
+				int speedString = simpleAction.getSpeedType();
+				switch (speedString)
+				{
+					case WorkoutAction.ACTION_SIMPLE_SPEED_SLOW:
+						holder.speedString.setText(context.getResources().getString(R.string.slow));
+						break;
+					case WorkoutAction.ACTION_SIMPLE_SPEED_STEADY:
+						holder.speedString.setText(context.getResources().getString(R.string.steady));
+						break;
+					case WorkoutAction.ACTION_SIMPLE_SPEED_FAST:
+						holder.speedString.setText(context.getResources().getString(R.string.fast));
+						break;
+				}
+				int valueType = simpleAction.getValueType();
+				switch (valueType)
+				{
+					case WorkoutAction.ACTION_SIMPLE_VALUE_TYPE_DISTANCE:
+						holder.timeOrDistance.setText(String.format("%.3f%s", simpleAction.getValue() / 1000, context
+							.getResources().getString(R.string.km)));
+						break;
+					case WorkoutAction.ACTION_SIMPLE_VALUE_TYPE_TIME:
+						holder.timeOrDistance
+							.setText(TimeFormatter.formatTimeHHMMSS((long) (simpleAction.getValue())));
+						break;
+				}
+				break;
+			
+			case WorkoutAction.ACTION_ADVANCED:
+				//TODO set image here
+				//holder.image.setImageResource(/*advanced image id*/);
+				WorkoutActionAdvanced advancedAction = (WorkoutActionAdvanced) action;
+				holder.time.setText(TimeFormatter.formatTimeHHMMSS(advancedAction.getTime()));
+				holder.pace.setText(TimeFormatter.formatTimeMMSSorHHMMSS(advancedAction.getPace()));
+				holder.distance.setText(String.format("%.3f", advancedAction.getDistance() / 1000));
+				break;
 		}
 		
 		return row;
