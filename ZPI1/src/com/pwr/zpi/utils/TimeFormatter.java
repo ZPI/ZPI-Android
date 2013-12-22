a840ea2676e1900d4920e8bbbda5fe836113c003
Kuba <Kuba@Kuba-Komputer>
2013-12-22 20:27:43 +0100

    pace cant be 5:60 now

--- ZPI1/src/com/pwr/zpi/utils/TimeFormatter.java
+++ ZPI1/src/com/pwr/zpi/utils/TimeFormatter.java
@@ -38,8 +38,8 @@ public class TimeFormatter {
 		rest = rest * 60;
 		int hours = (int) (pace / 60);
 		pace -= hours * 60;
-		if (hours == 0) return String.format("%d:%02d", (int) pace, Math.round(rest));
-		else return String.format("%02d:%02d:%02d", hours, (int) pace, Math.round(rest));
+		if (hours == 0) return String.format("%d:%02d", (int) pace, (int) rest);
+		else return String.format("%02d:%02d:%02d", hours, (int) pace, (int) rest);
 		
 	}
 	
